540414b3323c5dd39027f3c43f5821119a9bf6c9
Abdul Hakeem <abd_hakeem@outlook.com>
2022-03-19 17:25:01 +0800

    add breadcrumbs component

--- src/layouts/SidebarLayout/Breadcrumbs/index.tsx
+++ src/layouts/SidebarLayout/Breadcrumbs/index.tsx
@@ -1,46 +1,34 @@
-import { useContext } from 'react';
+import { Container, Link, Typography } from '@mui/material';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import WorkIcon from '@mui/icons-material/Work';
+import styles from './breadcrumbs.module.scss';
 
-import { Box, Hidden, IconButton, Tooltip } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
-import { SidebarContext } from 'src/contexts/SidebarContext';
-import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
-
-import Logo from 'src/components/Logo';
-import 'src/style.css';
-import { Grid } from 'ag-grid-community';
-
-const HeaderWrapper = styled(Box)(
-  ({ theme }) => `
-        height: 150px;
-        color: ${theme.header.textColor};
-        right: 0;
-        z-index: 5;
-        box-shadow: ${theme.header.boxShadow};
-        justify-content: space-between;
-        width: 100%;
-        padding: 50px 40px 0px 40px;
-        background: #f5f5f5;
-        color: #000;
-        
-        
-        @media (min-width: ${theme.breakpoints.values.lg}px) {
-            
-        }
-`
-);
-
-function Breadcrumbs() {
-  const { sidebarToggle, toggleSidebar } = useContext(SidebarContext);
-
+function BreadcrumbComp() {
   return (
-    <HeaderWrapper display="block" alignItems="center" className='Breadcrumbs-body'>
+    <Container className={styles.container}>
       <div>
-      <Box> <h2 className="Breadcrumb-heading">[CASE NAME]</h2> </Box>
-      <Box> <p className="Breadcrumb-link"> Cases &gt; 1 </p> </Box>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link
+            underline="hover"
+            sx={{ display: 'flex', alignItems: 'center' }}
+            color="inherit"
+            href="/"
+          >
+            <WorkIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            My Cases
+          </Link>
+          {/*<Link
+            underline="hover"
+            color="inherit"
+            href="/getting-started/installation/"
+          >
+            Core
+    </Link>*/}
+          <Typography color="text.primary">Breadcrumbs</Typography>
+        </Breadcrumbs>
       </div>
-    </HeaderWrapper> 
+    </Container>
   );
 }
 
-export default Breadcrumbs;
+export default BreadcrumbComp;
